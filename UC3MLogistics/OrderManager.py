fd4a9a092d545fce25266edc0ca16af72b6c9d82
Javier Pallarés <100472252@alumnos.uc3m.es>
2023-02-09 17:12:20 +0100

    Finished Validate function

--- UC3MLogistics/OrderManager.py
+++ UC3MLogistics/OrderManager.py
@@ -1,35 +1,40 @@
 import json
 import re
-##from .OrderMangementException import OrderManagementException
-##from .OrderRequest import OrderRequest
+from .OrderMangementException import OrderManagementException # TODO solve this
+from .OrderRequest import OrderRequest
 
 
 class OrderManager:
     def __init__(self):
         pass
 
-def ValidateEAN13(eAn13:str) -> bool:
+    def ValidateEAN13(eAn13:str) -> bool:
+        """This function validates an EAN13 barcode"""
+        valid_structure = (re.search("^[0-9]{13}$", eAn13))
+        if not valid_structure:
+            return False
 
+        digit_counter = 1
+        sum = 0
+        while digit_counter <= 12:
+            par_position = (digit_counter % 2 == 0)
+            digit = int(eAn13[digit_counter - 1])
+            if par_position:
+                sum += digit * 3
+            else:
+                sum += digit
+            digit_counter += 1
 
-    x = (re.search("^[0-9]{13}$", eAn13))
-    # PLEASE INCLUDE HERE THE CODE FOR VALIDATING THE GUID
-    # RETURN TRUE IF THE GUID IS RIGHT, OR FALSE IN OTHER CASE
-    digit_counter = 1
-    suma = 0
-    while digit_counter < 13:
-        par = digit_counter % 2 == 0
-        if not par:
-            suma += eAn13[digit_counter]
-        else:
-            suma += eAn13[digit_counter]*3
+        mod = sum % 10
+        check_digit = 10 - mod
+        if check_digit == 10:
+            check_digit = 0
 
-    if not x:
-        return False
-    print(x)
-    return True
-print(ValidateEAN13("1234567891123"))
+        if check_digit != int(eAn13[12]):
+            return False
 
-"""
+        return True
+
     def ReadproductcodefromJSON(self, fi: str) -> None: # DUDA AQUI PREGUNTAR
 
         try:
@@ -52,4 +57,3 @@ print(ValidateEAN13("1234567891123"))
 
         # Close the file
         return req
-        """
